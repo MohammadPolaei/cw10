a19c881193c008c643f82d776d3c7aab9da55f62
Saleh <hmohamad.sms@gmail.com>
2025-10-24 17:09:07 +0330

    feat(review): create review section

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { Header } from "./component/header/header.js";
 import { Hero } from "./component/hero/hero.js";
 import { IntroCards } from "./component/intro-cards/intro-cards.js";
+import { Reviews } from "./component/reviews/reviews.js";
 import "./style.css";
 
 const app = document.getElementById("app");
 
-app.append(Header(), Hero(), IntroCards());
+app.append(Header(), Hero(), IntroCards(), Reviews());
