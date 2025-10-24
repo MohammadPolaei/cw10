016b46c58ab5d1411db2bb432de5ceec94018cb2
zahra <z.godarzi75@gmail.com>
2025-10-24 17:17:57 +0330

    feat (productCard):creat product section

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import { Header } from "./component/header/header.js";
-import { Hero } from "./component/hero/hero.js";
-import { IntroCards } from "./component/intro-cards/intro-cards.js";
-import "./style.css";
+import { Header } from './component/header/header.js';
+import { Hero } from './component/hero/hero.js';
+import { IntroCards } from './component/intro-cards/intro-cards.js';
+import { ProductCard } from './component/productCard/productCard.js';
+import './style.css';
 
-const app = document.getElementById("app");
+const app = document.getElementById('app');
 
-app.append(Header(), Hero(), IntroCards());
+app.append(Header(), Hero(), IntroCards(), ProductCard());
